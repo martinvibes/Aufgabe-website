55f45aa76f298bfbf1d03c2c8905b732caabcc48
martin machiebe <martinmachiebe21@gmail.com>
2024-06-09 15:34:22 +0100

    changed the sentence of about section

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,10 +7,10 @@ function AboutUs() {
         <div className="">
           <h1 className="text-font32 font-bold text-white">About Us</h1>
           <p className="line my-3 text-font20 font-normal leading-9 text-white">
-            At Aufgabe, we are dedicated to providing top-tier cloud solutions{" "}
-            <br /> and cybersecurity services that empower businesses to thrive
+            At Aufgabe, we are dedicated to providing top-tier cloud solutions
+            and <br /> cybersecurity services that empower businesses to thrive
             in the digital <br /> age. With years of expertise and a commitment
-            to excellence, <br /> we help organizations navigate the
+            to excellence, we help <br /> organizations navigate the
             complexities of IT infrastructure with <br /> confidence and ease.
           </p>
           <button className="mt-5 w-heroBtnW rounded-lg border-2 bg-navBtn px-p20 py-p12 text-font20 font-medium text-white transition-all duration-200 hover:border-btnHover hover:bg-btnHover">
