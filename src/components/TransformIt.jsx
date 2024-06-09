485225700c52dd3c340edf342c70d6395ee6e31b
martin machiebe <martinmachiebe21@gmail.com>
2024-06-09 04:09:33 +0100

    removed the hover state on transform it section

--- src/components/TransformIt.jsx
+++ src/components/TransformIt.jsx
@@ -3,17 +3,17 @@ import heroSide from "../images/hero-side.png";
 function TransformIt() {
   return (
     <div className="relative mx-[4.5rem] mb-16 mt-32">
-      <div className="from-linear1 to-linear2 w-full rounded-lg bg-gradient-to-r py-44"></div>
+      <div className="w-full rounded-lg bg-gradient-to-r from-linear1 to-linear2 py-44"></div>
       <div className="absolute inset-0">
         <div className="py-8 text-center text-white">
-          <h1 className="text-font40 text-center font-bold">
+          <h1 className="text-center text-font40 font-bold">
             Ready to Transform Your It <br /> infrastructure?
           </h1>
-          <p className="text-font20 py-2 font-normal">
+          <p className="py-2 text-font20 font-normal">
             Partner with us for a reliable cloud solutions and robust
             cybersecurity.
           </p>
-          <button className="w-heroBtnW h-heroBtnH hover:bg-btnHover text-font20 bg-navBtn hover:border-btnHover mt-5 whitespace-nowrap rounded-lg border-2 font-medium transition-all duration-500 ease-in-out">
+          <button className="mt-5 h-heroBtnH w-heroBtnW whitespace-nowrap rounded-lg border-2 bg-navBtn text-font20 font-medium">
             Get Started
           </button>
         </div>
