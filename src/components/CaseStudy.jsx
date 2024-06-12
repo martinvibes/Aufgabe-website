90e74d0da3e9fb1d4e74d5ca7fde1f53c9405e94
martin machiebe <martinmachiebe21@gmail.com>
2024-06-12 22:58:39 +0100

    added responsiveness

--- src/components/CaseStudy.jsx
+++ src/components/CaseStudy.jsx
@@ -16,8 +16,10 @@ function CaseStudy() {
         />
         <div className="absolute inset-0 z-20 flex items-center justify-center">
           <div className="my-5 py-9 text-center text-white">
-            <h1 className="text-center text-font40 font-bold">Casestudies</h1>
-            <p className="py-2 text-font20 font-normal">
+            <h1 className="text-center text-[1.68rem] font-bold md:text-font40">
+              Casestudies
+            </h1>
+            <p className="py-1 text-[1rem] font-normal md:py-2 md:text-font20">
               Explore our case studies to learn about our successful projects.
             </p>
           </div>
@@ -30,14 +32,14 @@ function CaseStudy() {
 
 function CaseBox() {
   return (
-    <div className="mx-[5rem] my-12 grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+    <div className="mx-8 my-12 grid grid-cols-1 gap-10 md:mx-[5rem] md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
       <div className="group z-10 rounded-lg border p-7 transition-all duration-200 hover:bg-navBtn">
         <img className="" src={amdLogo} alt="icons logo" />
         <h1 className="text-font24 font-bold text-midBlack group-hover:text-white">
           Optimizing Cloud <br /> Infrastructure for Tech <br /> Innovations
           Inc.
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
@@ -61,7 +63,7 @@ function CaseBox() {
           Boosting Data Security and <br /> Compliance for HealthCare <br />{" "}
           Plus.
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
@@ -85,7 +87,7 @@ function CaseBox() {
           Improving Patient Care with IT <br /> Optimization for HealthNet{" "}
           <br /> Hospitals
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
@@ -109,7 +111,7 @@ function CaseBox() {
           Boosting Data Security and <br /> Compliance for HealthCare <br />{" "}
           Plus.
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
@@ -133,7 +135,7 @@ function CaseBox() {
           Improving Patient Care with IT <br /> Optimization for HealthNet{" "}
           <br /> Hospitals
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
@@ -157,7 +159,7 @@ function CaseBox() {
           Optimizing Cloud <br /> Infrastructure for Tech <br /> Innovations
           Inc.
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
@@ -181,7 +183,7 @@ function CaseBox() {
           Improving Patient Care with IT <br /> Optimization for HealthNet{" "}
           <br /> Hospitals
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
@@ -205,7 +207,7 @@ function CaseBox() {
           Optimizing Cloud <br /> Infrastructure for Tech <br /> Innovations
           Inc.
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
@@ -229,7 +231,7 @@ function CaseBox() {
           Boosting Data Security and <br /> Compliance for HealthCare <br />{" "}
           Plus.
         </h1>
-        <p className="text-font20 font-normal leading-9 text-midBlack group-hover:text-white">
+        <p className="text-font20 font-normal leading-snug text-midBlack group-hover:text-white md:leading-9">
           Tech Innovations Inc. conducted a <br /> comprehensive assessment and{" "}
           <br />
           migrated their entire infrastructure <br /> to a scalable cloud
