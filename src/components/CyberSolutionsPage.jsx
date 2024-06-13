205c6891ec75ba85bcc3c5b1c50b66890e78cc3a
martin machiebe <martinmachiebe21@gmail.com>
2024-06-13 16:30:07 +0100

    changed the hero image for mobile device

--- src/components/CyberSolutionsPage.jsx
+++ src/components/CyberSolutionsPage.jsx
@@ -1,4 +1,5 @@
 import cyberHero from "../images/solutionsHero.svg";
+import cyberChange from "../images/cyber-change.jpg";
 import cyberServices from "../images/cyber-services.svg";
 import pause from "../images/pause.svg";
 import approachImage from "../images/approach-image.svg";
@@ -11,18 +12,23 @@ const CyberSolutionsPage = () => {
         <div className="absolute inset-0 z-10 bg-linear2 opacity-75"></div>
         {/* Image */}
         <img
-          className="relative z-0 mt-20 w-full"
+          className="relative z-0 mt-20 w-full sm:hidden"
+          src={cyberChange}
+          alt="hero section"
+        />
+        <img
+          className="relative z-0 mt-20 hidden w-full sm:block"
           src={cyberHero}
           alt="hero section"
         />
         {/* Text Overlay */}
         <div className="absolute inset-0 z-20 flex items-center justify-start">
-          <div className="z-10 my-28 ml-[72px] py-9 text-white">
-            <h1 className="text-font40 font-bold">
+          <div className="z-10 my-28 px-8 text-white md:ml-[72px] md:py-9">
+            <h1 className="sm5:font-bold text-[1.5rem] font-extrabold md:text-4xl">
               Enhance Your Security with Our <br /> Cybersecurity Solutions.
             </h1>
-            <p className="py-2 text-font20 font-normal">
-              Protect Your Business from Cyber Threats with Our <br />{" "}
+            <p className="py-1 text-[1.14rem] font-normal md:py-2 md:text-font20">
+              Protect Your Business from Cyber Threats with Our <br />
               Comprehensive Security Services
             </p>
           </div>
@@ -38,32 +44,34 @@ const CyberSolutionsPage = () => {
 
 function CyberMatters() {
   return (
-    <div className="my-16 flex items-center justify-center">
-      <div className="">
+    <div className="my-12 flex items-center justify-center md:my-16">
+      <div className="px-8">
         <div className="text-center">
-          <h1 className="text-font32 font-bold">Why CyberSeurity Matters</h1>
-          <p className="mb-7 text-font24 text-trustedH1">
+          <h1 className="text-[1.8rem] font-bold md:text-font32">
+            Why CyberSeurity Matters
+          </h1>
+          <p className="mb-7 text-[1.45rem] text-trustedH1 md:text-font24">
             Innovative solutions to secure and empower your business.
           </p>
         </div>
-        <div className="mx-8 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 lg:gap-10">
+        <div className="grid grid-cols-1 md:mx-8 md:grid-cols-1 lg:grid-cols-2 lg:gap-10">
           <div>
             <p className="text-font20 font-normal leading-9 text-midBlack">
-              In today&apos;s digital landscape, cybersecurity is paramount. As
-              businesses <br /> increasingly rely on digital platforms and
-              data-driven processes, they <br /> face a growing array of cyber
-              threats. From data breaches to <br /> ransomware attacks, these
-              threats can inflict severe financial and <br /> reputational
-              damage.
+              <b>In today&apos;s</b> digital landscape, cybersecurity is
+              paramount. As businesses <br /> increasingly rely on digital
+              platforms and data-driven processes, they <br /> face a growing
+              array of cyber threats. From data breaches to <br /> ransomware
+              attacks, these threats can inflict severe financial and <br />{" "}
+              reputational damage.
             </p>
           </div>
           <div>
             <p className="pt-4 text-font20 font-normal leading-9 text-midBlack lg:pt-0">
-              Investing in cybersecurity is not just about protecting data,
-              it&apos;s about <br /> safeguarding the future of your business.
-              By implementing robust <br /> security measures, you can mitigate
-              risks, detect threats, and ensure <br /> business continuity in
-              the face of evolving cyber threats.
+              <b>Investing</b> in cybersecurity is not just about protecting
+              data, it&apos;s about <br /> safeguarding the future of your
+              business. By implementing robust <br /> security measures, you can
+              mitigate risks, detect threats, and ensure <br /> business
+              continuity in the face of evolving cyber threats.
             </p>
           </div>
         </div>
@@ -83,18 +91,18 @@ function CyberServices() {
           alt="hero section"
         />
 
-        <div className="absolute inset-0 pt-24 text-white lg:text-center">
-          <h1 className="text-center text-font32 font-bold lg:text-font40">
+        <div className="absolute inset-0 pt-12 text-white md:pt-24 lg:text-center">
+          <h1 className="text-center text-[1.7rem] font-bold md:text-font32 lg:text-font40">
             Our Cybersecurity Services
           </h1>
-          <p className="py-2 text-center text-font20 font-normal">
+          <p className="py-2 text-center text-[1.18rem] font-normal md:text-font20">
             Innovative solutions to secure and empower <br /> your business.
           </p>
         </div>
       </div>
 
       <div className="">
-        <div className="m-10 grid grid-cols-2 gap-10 pt-12 text-font20">
+        <div className="m-10 grid grid-cols-2 gap-10 pt-2 text-font20 md:pt-12">
           <div className="flex items-center">
             <img className="mr-2" src={pause} alt="pause icon" />
             <h2>Network Security</h2>
@@ -130,8 +138,10 @@ function OurApproach() {
     <div className="mx-auto my-10 flex items-center justify-center">
       <div>
         <div className="text-center">
-          <h1 className="text-font32 font-bold">Our Approach</h1>
-          <p className="text-font24 text-trustedH1">
+          <h1 className="text-[1.78rem] font-bold md:text-font32">
+            Our Approach
+          </h1>
+          <p className="text-[1.34rem] text-trustedH1 md:text-font24">
             Our approach to protecting your business.
           </p>
         </div>
@@ -216,17 +226,17 @@ function FourBoxesWithImage() {
 
 function TransformIt() {
   return (
-    <div className="relative mx-[4.5rem] mb-16 mt-12">
-      <div className="w-full rounded-lg bg-gradient-to-r from-linear1 to-linear2 py-32"></div>
+    <div className="relative mx-8 mb-16 md:mx-[4.5rem] md:mt-32">
+      <div className="w-full rounded-lg bg-gradient-to-r from-linear1 to-linear2 py-44"></div>
       <div className="absolute inset-0">
         <div className="py-8 text-center text-white">
-          <h1 className="text-center text-font40 font-bold">
-            Ready To Secure Your Business?
+          <h1 className="text-center text-[1.7rem] font-bold md:text-font40">
+            Ready to Secure Your Bussines?
           </h1>
-          <p className="py-2 text-font20 font-normal">
+          <p className="text-font20 font-normal md:py-2">
             Contact Us Today to Learn More About Our Cybersecurity Services
           </p>
-          <button className="mt-5 h-heroBtnH w-heroBtnW whitespace-nowrap rounded-lg border-2 text-font20 font-medium transition-all duration-500 ease-in-out hover:border-btnHover hover:bg-btnHover">
+          <button className="mt-5 whitespace-nowrap rounded-lg border-2 bg-navBtn p-0 px-4 py-2 text-font20 font-medium md:h-heroBtnH md:w-heroBtnW">
             Get Started
           </button>
         </div>
